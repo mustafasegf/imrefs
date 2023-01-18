2e48fbd9bf92b2d5f1a580dfa10a764984344aeb
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-01-18 18:29:09 +0700

    feat: use linux clone

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,83 @@
+use nix::sched::{clone, CloneFlags};
 use std::env::args;
 use std::fs::{self, File, OpenOptions};
 use std::io::prelude::*;
 use std::os::unix::net::{UnixListener, UnixStream};
-use std::path::Path;
-use std::process::{exit, Command};
-// use std::process::Stdio;
+use std::process::exit;
+
+const STACK_SIZE: usize = 1024 * 1024;
+
+fn daemon(name: String) -> isize {
+    let socket_path = format!("/tmp/imrefs-{}.sock", name);
+    let file_name = format!("/tmp/imrefs-{}.tmp", name);
+
+    if std::fs::metadata(&socket_path).is_ok() {
+        if let Err(e) = fs::remove_file(&socket_path) {
+            println!("Error can't remove socket: {}", e);
+            exit(1);
+        };
+    }
+
+    let listener = match UnixListener::bind(&socket_path) {
+        Ok(l) => l,
+        Err(_) => {
+            println!("Error can't bind socket");
+            exit(1);
+        }
+    };
+
+    for stream in listener.incoming() {
+        let mut stream = match stream {
+            Ok(stream) => stream,
+            Err(e) => {
+                println!("Error: {}", e);
+                continue;
+            }
+        };
+        let mut response = String::new();
+        if let Err(e) = stream.read_to_string(&mut response) {
+            println!("Error can't read from socket: {}", e);
+            continue;
+        };
+
+        if response == "cmd:stop" {
+            if let Err(e) = fs::remove_file(&file_name) {
+                println!("Error can't remove file: {}", e);
+                exit(1);
+            };
+            if let Err(e) = fs::remove_file(&socket_path) {
+                println!("Error can't remove file: {}", e);
+                exit(1);
+            };
+
+            println!("Filesystem {} successfully removed", &name);
+            exit(0);
+        }
+
+        response = response.replace("msg:", "");
+
+        let mut file = match OpenOptions::new()
+            .create(true)
+            .write(true)
+            .truncate(true)
+            .open(&file_name)
+        {
+            Ok(f) => f,
+            Err(e) => {
+                println!("Error can't open file: {}", e);
+                exit(1);
+            }
+        };
+
+        if let Err(e) = file.write_all(response.as_bytes()) {
+            println!("Error can't write to file: {}", e);
+            continue;
+        };
+
+        println!("Data successfully written to file: {}", &file_name);
+    }
+    0
+}
 
 fn main() {
     let args = args().collect::<Vec<_>>();
@@ -24,18 +97,24 @@ fn main() {
                 println!("Usage: executable init [name]");
                 exit(1);
             }
+
             let name = args[2].clone();
+            let ref mut stack: [u8; STACK_SIZE] = [0; STACK_SIZE];
 
-            let Ok(child) = Command::new("/proc/self/exe")
-                .arg("child")
-                .arg(&name)
-                // .stdout(Stdio::null())
-                .spawn() else {
-                    println!("Failed to spawn child");
+            let pid = match clone(
+                Box::new(|| daemon(name.clone())),
+                stack,
+                CloneFlags::empty(),
+                None,
+            ) {
+                Ok(pid) => pid,
+                Err(e) => {
+                    println!("Failed to spawned children: {}", e);
                     exit(1);
-                };
+                }
+            };
 
-            let file_name = format!("/tmp/imrefs-{}.tmp", name);
+            let file_name = format!("/tmp/imrefs-{}.tmp", &name);
             if let Err(e) = File::create(&file_name) {
                 println!("Error can't create file: {}", e);
                 exit(1);
@@ -43,18 +122,16 @@ fn main() {
 
             println!(
                 "Filesystem {} successfully created at {} with PID {}",
-                &name,
-                &file_name,
-                child.id()
+                &name, &file_name, pid
             );
         }
         "send" => {
-            if args.len() != 4 {
+            if args.len() < 4 {
                 println!("Usage: executable send [name] [message]");
                 exit(1);
             }
             let name = args[2].clone();
-            let message = format!("msg:{}", args[3].clone());
+            let message = format!("msg:{}", &args[3..].join(" "));
             let socket_path = format!("/tmp/imrefs-{}.sock", name);
             let mut stream = match UnixStream::connect(&socket_path) {
                 Ok(s) => s,
@@ -91,89 +168,6 @@ fn main() {
             };
         }
 
-        "child" => {
-            if args.len() != 3 {
-                println!("Usage: executable child [name]");
-                exit(1);
-            }
-            let name = args[2].clone();
-            let socket_path = format!("/tmp/imrefs-{}.sock", name);
-            let file_name = format!("/tmp/imrefs-{}.tmp", name);
-
-            if std::fs::metadata(&socket_path).is_ok() {
-                if let Err(e) = fs::remove_file(&socket_path) {
-                    println!("Error can't remove socket: {}", e);
-                    exit(1);
-                };
-            }
-
-            let listener = match UnixListener::bind(&socket_path) {
-                Ok(l) => l,
-                Err(_) => {
-                    println!("Error can't bind socket");
-                    exit(1);
-                }
-            };
-
-            for stream in listener.incoming() {
-                let mut stream = match stream {
-                    Ok(stream) => stream,
-                    Err(e) => {
-                        println!("Error: {}", e);
-                        continue;
-                    }
-                };
-                let mut response = String::new();
-                if let Err(e) = stream.read_to_string(&mut response) {
-                    println!("Error can't read from socket: {}", e);
-                    continue;
-                };
-
-                if response == "cmd:stop" {
-                    if let Err(e) = fs::remove_file(&file_name) {
-                        println!("Error can't remove file: {}", e);
-                        exit(1);
-                    };
-                    if let Err(e) = fs::remove_file(&socket_path) {
-                        println!("Error can't remove file: {}", e);
-                        exit(1);
-                    };
-
-                    println!("Filesystem {} successfully removed", &name);
-                    exit(0);
-                }
-
-                response = response.replace("msg:", "");
-
-                if Path::new(&file_name).exists() {
-                    if let Err(e) = fs::remove_file(&file_name) {
-                        println!("Error can't remove file: {}", e);
-                        continue;
-                    };
-                }
-
-                let mut file = match OpenOptions::new()
-                    .create_new(true)
-                    .write(true)
-                    .append(true)
-                    .open(&file_name)
-                {
-                    Ok(f) => f,
-                    Err(e) => {
-                        println!("Error can't open file: {}", e);
-                        exit(1);
-                    }
-                };
-
-                if let Err(e) = file.write_all(response.as_bytes()) {
-                    println!("Error can't write to file: {}", e);
-                    continue;
-                };
-
-                println!("Data successfully written to file: {}", &file_name);
-                exit(0);
-            }
-        }
         _ => {
             println!("Usage: executable [command] where command is one of init, send, stop");
             exit(1);
