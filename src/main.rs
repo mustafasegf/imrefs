f6365183cc45f0f0b35709e22f5caa915efc30b8
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2023-01-18 19:15:06 +0700

    feat: added signal processing handle

--- src/main.rs
+++ src/main.rs
@@ -2,80 +2,118 @@ use nix::sched::{clone, CloneFlags};
 use std::env::args;
 use std::fs::{self, File, OpenOptions};
 use std::io::prelude::*;
-use std::os::unix::net::{UnixListener, UnixStream};
+use std::os::unix::net::UnixStream;
 use std::process::exit;
+
+use futures::stream::StreamExt;
+use signal_hook::consts::signal::*;
+use signal_hook_tokio::Signals;
+
+use tokio::io::AsyncReadExt;
+use tokio::net::UnixListener as AsyncUnixListener;
+use tokio::runtime::Runtime;
 
 const STACK_SIZE: usize = 1024 * 1024;
 
 fn daemon(name: String) -> isize {
-    let socket_path = format!("/tmp/imrefs-{}.sock", name);
-    let file_name = format!("/tmp/imrefs-{}.tmp", name);
+    let rt = Runtime::new().unwrap();
 
-    if std::fs::metadata(&socket_path).is_ok() {
-        if let Err(e) = fs::remove_file(&socket_path) {
-            println!("Error can't remove socket: {}", e);
-            exit(1);
-        };
-    }
+    let socket_name_out = format!("/tmp/imrefs-{}.sock", name);
+    let file_name_out = format!("/tmp/imrefs-{}.tmp", name);
 
-    let listener = match UnixListener::bind(&socket_path) {
-        Ok(l) => l,
-        Err(_) => {
-            println!("Error can't bind socket");
-            exit(1);
+    rt.block_on(async move {
+        let mut signals = Signals::new(&[SIGHUP, SIGTERM, SIGINT, SIGQUIT]).unwrap();
+        let _handle = signals.handle();
+
+        let socket_name = format!("/tmp/imrefs-{}.sock", name);
+        let file_name = format!("/tmp/imrefs-{}.tmp", name);
+
+        if std::fs::metadata(&socket_name).is_ok() {
+            if let Err(e) = fs::remove_file(&socket_name) {
+                println!("Error can't remove socket: {}", e);
+                exit(1);
+            };
         }
+
+        let listener = match AsyncUnixListener::bind(&socket_name) {
+            Ok(l) => l,
+            Err(_) => {
+                println!("Error can't bind socket");
+                exit(1);
+            }
+        };
+
+        'main: loop {
+            tokio::select! {
+                _signal =signals.next() => {
+                    println!("Signal received");
+                    break 'main;
+                }
+
+                stream =listener.accept() => {
+                    let mut stream = match stream {
+                        Ok(stream) => stream.0,
+                        Err(e) => {
+                            println!("Error: {}", e);
+                            continue;
+                        }
+                    };
+                    let mut response = String::new();
+                    if let Err(e) = stream.read_to_string(&mut response).await {
+                        println!("Error can't read from socket: {}", e);
+                        continue;
+                    };
+
+                    if response == "cmd:stop" {
+                        if let Err(e) = fs::remove_file(&file_name) {
+                            println!("Error can't remove file: {}", e);
+                            exit(1);
+                        };
+                        if let Err(e) = fs::remove_file(&socket_name) {
+                            println!("Error can't remove file: {}", e);
+                            exit(1);
+                        };
+
+                        println!("Filesystem {} successfully removed", &name);
+                        exit(0);
+                    }
+
+                    response = response.replace("msg:", "");
+
+                    let mut file = match OpenOptions::new()
+                        .create(true)
+                        .write(true)
+                        .truncate(true)
+                        .open(&file_name)
+                    {
+                        Ok(f) => f,
+                        Err(e) => {
+                            println!("Error can't open file: {}", e);
+                            exit(1);
+                        }
+                    };
+
+                    if let Err(e) = file.write_all(response.as_bytes()) {
+                        println!("Error can't write to file: {}", e);
+                        continue;
+                    };
+
+                    println!("Data successfully written to file: {}", &file_name);
+                }
+            }
+        }
+    });
+
+    if let Err(e) = fs::remove_file(&socket_name_out) {
+        println!("Error can't remove socket: {}", e);
+        return 1;
     };
 
-    for stream in listener.incoming() {
-        let mut stream = match stream {
-            Ok(stream) => stream,
-            Err(e) => {
-                println!("Error: {}", e);
-                continue;
-            }
-        };
-        let mut response = String::new();
-        if let Err(e) = stream.read_to_string(&mut response) {
-            println!("Error can't read from socket: {}", e);
-            continue;
-        };
+    if let Err(e) = fs::remove_file(&file_name_out) {
+        println!("Error can't remove temporary file: {}", e);
+        return 1;
+    };
 
-        if response == "cmd:stop" {
-            if let Err(e) = fs::remove_file(&file_name) {
-                println!("Error can't remove file: {}", e);
-                exit(1);
-            };
-            if let Err(e) = fs::remove_file(&socket_path) {
-                println!("Error can't remove file: {}", e);
-                exit(1);
-            };
-
-            println!("Filesystem {} successfully removed", &name);
-            exit(0);
-        }
-
-        response = response.replace("msg:", "");
-
-        let mut file = match OpenOptions::new()
-            .create(true)
-            .write(true)
-            .truncate(true)
-            .open(&file_name)
-        {
-            Ok(f) => f,
-            Err(e) => {
-                println!("Error can't open file: {}", e);
-                exit(1);
-            }
-        };
-
-        if let Err(e) = file.write_all(response.as_bytes()) {
-            println!("Error can't write to file: {}", e);
-            continue;
-        };
-
-        println!("Data successfully written to file: {}", &file_name);
-    }
     0
 }
 
@@ -132,8 +170,8 @@ fn main() {
             }
             let name = args[2].clone();
             let message = format!("msg:{}", &args[3..].join(" "));
-            let socket_path = format!("/tmp/imrefs-{}.sock", name);
-            let mut stream = match UnixStream::connect(&socket_path) {
+            let socket_name = format!("/tmp/imrefs-{}.sock", name);
+            let mut stream = match UnixStream::connect(&socket_name) {
                 Ok(s) => s,
                 Err(_) => {
                     println!("Filesystem {} not found", &name);
@@ -153,8 +191,8 @@ fn main() {
             }
 
             let name = args[2].clone();
-            let socket_path = format!("/tmp/imrefs-{}.sock", name);
-            let mut stream = match UnixStream::connect(&socket_path) {
+            let socket_name = format!("/tmp/imrefs-{}.sock", name);
+            let mut stream = match UnixStream::connect(&socket_name) {
                 Ok(s) => s,
                 Err(_) => {
                     println!("Filesystem {} not found", &name);
